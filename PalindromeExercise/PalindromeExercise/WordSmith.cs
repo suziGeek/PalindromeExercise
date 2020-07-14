8638177dd8fa453caa475eccd873797bafbb8b5e
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-07-14 15:14:53 -0400

    added case insensitivity to test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+       
+        public bool IsAPalindrome(string word)
+        {
+            return word.ToLower().SequenceEqual(word.Reverse());
+        }
+        
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,21 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("something", false)]
 
+        public void Test1(string word, bool expected)
+        {
+            //arrange
+            var wordsmith = new WordSmith();
+
+            //act
+            var actual = wordsmith.IsAPalindrome(word);
+
+            //assert
+
+            Assert.Equal(expected, actual);
         }
     }
 }
